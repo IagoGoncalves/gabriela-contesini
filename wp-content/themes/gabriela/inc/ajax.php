c149d0ddb4b94f61b6eab9fda52917d5916355ee
IagoGoncalves <iago_375@hotmail.com>
2023-03-29 13:55:19 -0300

    email 90% funcionando

--- wp-content/themes/gabriela/inc/ajax.php
+++ wp-content/themes/gabriela/inc/ajax.php
@@ -7,52 +7,37 @@ function my_action_enviar_email() {
 
 	if (($_POST['tipo-email']) == "contato"){
 		$deNome = $_POST['name'];
-		$deEmail = $_POST['email'];
-		$deTelefone = $_POST['telefone'];
 		$deAssunto = $_POST['assunto'];
-		$deMensagem = $_POST['message'];
-
-		$mensagem = 
-			"Contato Gabriela Contesini\n\n" . 
-			"Nome: " . $deNome . "\n" .
-			"E-mail: " . $deEmail . "\n" .
-			"Telefone: " . $deTelefone . "\n" .
-			"Assunto: " . $deAssunto . "\n" .
-			"Mensagem: " . $deMensagem . "\n" .
-			"Mensagem Enviada pelo Site Gabriela Contesini";
+		$deEmail = $_POST['email'];
+		$deTelefone  = $_POST['telefone'];
+		$demensagem  = $_POST['message'];
+	    
+	    $arquivo = 
+			"Contato Gabriela Contesini \n\n". 
+			"Nome: " . $deNome . "\n".
+			"Assunto: " . $deAssunto ."\n".
+			"Email: " . $deEmail ."\n".
+			"Telefone: " . $deTelefone ."\n".
+			"Mensagem: " . $demensagem ."\n\n".
+			"Email enviado pelo formulario do Site Gabriela Contesini";
 	}
 
-	$mailer = new PHPMailer();
-	$mailer->IsSMTP();
-	$mailer->SMTPDebug = 1;
-	$mailer->Port = 587;
-	$mailer->Host = 'localhost';
-
-	//$mailer->SMTPAuth = true; //Define se haverá ou não autenticação no SMTP
-	//$mailer->Username = ''; //Informe o e-mail o completo
-	//$mailer->Password = ''; //Senha da caixa postal
-
-	// DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
-	date_default_timezone_set('America/Sao_Paulo');
-
-	$mailer->FromName = $deNome; //Nome que será exibido para o destinatário
-	$mailer->From = $deEmail; //Obrigatório ser a mesma caixa postal indicada em "username"
-	$mailer->AddReplyTo($deEmail,$deNome);
-	$mailer->AddAddress('iago@ideapublicidade.com.br'); //Destinatários
-
-	//Conversor UTF-8 para acentuação
-	//$assunto = "Página Orçamento";
-	$mailer->Subject = $assunto = '=?UTF-8?B?'.base64_encode($assunto).'?=';		
-	$mailer->Subject = "E-mail do Site Gabriela Contesini" ." - ".date("H:i").'-'.date("d/m/Y");
-	$mailer->Body = $mensagem;
-	$mailer->CharSet = "UTF-8";
-
-	if($mailer->Send()) { ?>
-		<h3 class='enviado'>Mensagem enviada com sucesso.</h3>	
-	<?php }
-
-	else { ?>
-		<h3 class='erro'>A mensagem não pode ser enviada, tente novamente ou tente mais tarde </h3>
-	<?php }	
+	$destino = "contato@gabrielacontesini.com.br";		
+	$remetente = $deEmail;
+	$subject = $deAssunto;
+	$origem = $_POST['from'];
+	$headers = "MIME-Version: 1.1\n";
+	$headers .= "Content-type: text/plain; charset=UTF-8\n";
+	$nomeRemetente = "Nome personalizado";
+	$headers .= "From: " . $nomeRemetente . " <" . $remetente . ">\n";	
+	
+	$headers .= "Reply-To: " . $origem . "\n";
+	
+	if(mail($destino, $subject, $arquivo, $headers)){
+		echo "Mensagem enviada com sucesso " . $msg;
+	} else {
+		echo "A mensagem não pode ser enviada, tente novamente ou tente mais tarde";		
+	};
+	
 	die();
 }
